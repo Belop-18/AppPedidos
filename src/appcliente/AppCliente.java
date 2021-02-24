2e67706ad783c6e0d0c1caa4546567bcfde76cfd
Belop-18 <belop_18@hotmail.com>
2021-02-24 20:06:47 -0300

    Creación proyecto AppCliente

---
+++ src/appcliente/AppCliente.java
@@ -0,0 +1,16 @@
+package appcliente;
+
+/**
+ *
+ * @author Ana
+ */
+public class AppCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FrmPedidoCliente().setVisible(true);
+    }
+    
+}
